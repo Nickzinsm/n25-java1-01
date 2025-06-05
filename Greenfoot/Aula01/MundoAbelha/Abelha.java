7d23efbd65286dea7680fe0b8a6b7912547aa55e
Nickolas Maciel <jonaskakashi13@gmail.com>
2025-06-04 21:59:41 -0300

    Greenfoot Aula 02

---
+++ Greenfoot/Aula01/MundoAbelha/Abelha.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Esta é uma classe que representara uma abelha.
+ * 
+ * @author (Nickolas) 
+ * @version (2025-06-04)
+ */
+public class Abelha extends Actor
+{
+    /**
+     * Act - do whatever the Abelha wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        move(1);
+        if (Greenfoot.isKeyDown("left")){
+            turn(-5);
+        }
+        if (Greenfoot.isKeyDown("right")){
+            turn(5);
+
+        }
+    }
+
+}
